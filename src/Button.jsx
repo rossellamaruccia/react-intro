e76ceb042d734e05343522c50b95f85fe92fd19f
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-11-17 16:11:34 +0100

    folder set

---
+++ src/Button.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Component } from 'react'
+
+class NewButton extends Component {
+    render() {
+        return (
+            <button>
+                Click this button
+            </button>
+        )
+    }
+}
+
+export default NewButton
